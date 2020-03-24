9a424af3893d06f4130ecc6033a784996ff5b801
Mack McQuen <mackmcquen@comcast.net>
2020-03-24 23:50:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -49,6 +49,5 @@ end
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
    index_number = 4
-   
    array[index_number]
 end
